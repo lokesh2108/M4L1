71d69a3435cdb6a6a2affbab72bd17f32b12d057
lokesh2108 <lokil4980@gmail.com>
2023-02-18 22:57:11 -0800

    init

--- js/script.js
+++ js/script.js
@@ -10,24 +10,51 @@ var empcount = $("empCount");
 var c = 0;
 empcount.innerHTML = "(" + c + ")";
 
-// ADD EMPLOYEE
-form.addEventListener('submit', (e) => {
-    // PREVENT FORM SUBMISSION
-    var required,msg,id,ext,name,email,department;
-    required = "<span>Required Field<span>";
-    
 
+function addmulti(id,nam,ext,email){
+
+
+var distable = $("employees");
+    var row = distable.insertRow(-1);
+    var cell = row.insertCell(-1);
+    cell.innerHTML = id;
+    cell = row.insertCell(-1);
+    cell.innerHTML = nam;
+    cell = row.insertCell(-1);
+    cell.innerHTML = ext;
+    cell = row.insertCell(-1);
+    cell.innerHTML = email;
+    cell = row.insertCell(-1);
+    var deleteButton = document.createElement("BUTTON");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.setAttribute("class","del");
+    deleteButton.setAttribute("onclick", "deleteEmployee(this);");
+    cell.appendChild(deleteButton);
+}
+// ADD EMPLOYEE
+
+$("addForm").addEventListener('submit', (e) => {
+    // PREVENT FORM SUBMISSION
+    
+    e.preventDefault();
 
     // GET THE VALUES FROM THE TEXT BOXES
 
     id = $("id").value;
-    name = $("name").value;
+    nam = $("name").value;
     ext = $("extension").value;
     email = $("email").value;
 
+    //window.document.log(id); 
+
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
 
-    
+    // $("but").addEventListener("click",function(e) {
+    //     e.preventDefault();
+    //     addmulti(id,nam,ext,email);
+    // });
+    addmulti(id,nam,ext,email);
+    //addmulti();
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
 
